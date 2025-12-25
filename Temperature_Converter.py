b204ee13e96e55f83aaa6e046ba1ee3579f64965
A1r1p1it <105944189+A1r1p1it@users.noreply.github.com>
2025-12-25 23:08:13 +0530

    Delete Temperature_Converter.py
    Delete message

--- Temperature_Converter.py
+++
@@ -1,6 +0,0 @@
-### Problem 2: Temperature Converter
-# Create an array of 10 random temperatures in Celsius (between 0-40).
-# Convert them all to Fahrenheit using the formula: F = (C × 9/5) + 32
-a = np.random.randint(0, 41, size = 10)
-F = (a * 9/5) + 32
-print("Temperatures in Fahrenheit: ", F)

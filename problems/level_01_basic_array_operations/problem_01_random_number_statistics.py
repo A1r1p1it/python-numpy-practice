528da67d74bfaa3f454e79e66a99696aa0325935
A1r1p1it <105944189+A1r1p1it@users.noreply.github.com>
2025-12-25 22:43:49 +0530

    Add random number statistics script
    This script generates a 1D array of 100 random integers between 1-50 and calculates the mean, median, standard deviation, counts numbers above the mean, and finds the index of the maximum value.

---
+++ problems/level_01_basic_array_operations/problem_01_random_number_statistics.py
@@ -0,0 +1,19 @@
+### Problem 1: Random Number Statistics
+# Create a 1D array of 100 random integers between 1-50. Find:
+# - Mean, median, and standard deviation
+# - How many numbers are above the mean
+# - The index of the maximum value
+mport numpy as np
+a = np.random.randint(1, 51, size = 100)
+
+rand_mean = np.mean(a)
+rand_median = np.median(a)
+rand_std = np.std(a)
+
+print("Mean: ", rand_mean)
+print("Median: ", rand_median)
+print("Standard deviation: ", rand_std, "\n")
+
+b = print("Numbers above mean: ", a[a > rand_mean], "\n")
+c = np.argmax(a)
+print("Index value of max value: ", c)

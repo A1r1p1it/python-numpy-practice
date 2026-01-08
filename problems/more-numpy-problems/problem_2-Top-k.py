02f34af4683d73e136ab772e440139de0eddbaae
Arpit Kumar <105944189+A1r1p1it@users.noreply.github.com>
2026-01-08 16:33:43 +0530

    Top-k problem solution using NumPy
    Implement a solution to find the top 5 largest values in a random array without full sorting, using np.argpartition.

---
+++ problems/more-numpy-problems/problem_2-Top-k.py
@@ -0,0 +1,22 @@
+## Problem 2: Top-k Using Sorting 
+Create a 1D array of 30 random integers between 1 and 500.
+**Tasks:**
+- Find the 5 largest values WITHOUT using `np.sort` on the full
+array
+- Hint: Use `np.argpartition()`
+- Return them sorted in descending order
+- Also return their original indices
+arr = np.random.randint(1, 500, 30)
+print("30 random in: ", arr, "\n")
+
+indices = np.argpartition(arr, -5)[-5:]
+print("Indices of the last 5 values: ", indices, "\n")
+
+largest_5 = arr[indices]
+print("5 largest numbers: ", largest_5, "\n")
+
+sorting_order = np.sort(largest_5)[::-1]
+print("Descending order: ", sorting_order, "\n")
+
+orginal_indices = indices[np.argsort(largest_5)[::-1]]
+print("Orginal indices: ", orginal_indices, "\n")

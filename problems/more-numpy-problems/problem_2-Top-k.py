b27be6fea7aa8ef3dadbd104029ee8f17b0d1225
Arpit Kumar <105944189+A1r1p1it@users.noreply.github.com>
2026-01-08 23:03:03 +0530

    Top-k

--- problems/more-numpy-problems/problem_2-Top-k.py
+++ problems/more-numpy-problems/problem_2-Top-k.py
@@ -6,19 +6,19 @@
 # - Hint: Use `np.argpartition()`
 # - Return them sorted in descending order
 # - Also return their original indices
-import numpy as np
 arr = np.random.randint(1, 500, 30)
-print("30 random integers: ", arr, "\n")
+print("30 random in: ", arr, "\n")
 
 indices = np.argpartition(arr, -5)[-5:]
-print("Indices of the largest 5 :", indices)
+print("Indices of the last 5 values: ", indices, "\n")
 
 largest_5 = arr[indices]
-print("5 largest value: ", largest_5)
+print("5 largest numbers: ", largest_5, "\n")
 
 sorting_order = np.sort(largest_5)[::-1]
-print("Descending order of the 5 largest numbers: ", sorting_order)
+print("Descending order: ", sorting_order, "\n")
 
-orginal_indices = indices[np.argsort(largest_5)[::-1]]
-print("og indices in descending order: ", orginal_indices)
+sort_idx = np.argsort(largest_5)[::-1]
+orginal_indices = indices[sort_idx]
+print("Orginal indices: ", orginal_indices, "\n")
 
